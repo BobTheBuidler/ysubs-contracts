1cc4140ab95218452a14f7d8934d830dfe0282e9
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2023-04-05 02:26:42 -0400

    chore: refactor only_owner asserts (#1)

--- tests/test_owner_methods.py
+++ tests/test_owner_methods.py
@@ -9,7 +9,7 @@ def test_create_plan(subscriber_contract, accounts):
     owner, bad_guy = accounts[0], accounts[1]
     for i in range(1, MAX_POOL_CT):
         amount = i*10**18
-        with brownie.reverts("You are not the owner."):
+        with brownie.reverts("Only contract owner can call this function."):
             subscriber_contract.create_plan("Test Plan", amount, RATE_LIMIT, TIME_INTERVAL, _from(bad_guy))
         subscriber_contract.create_plan("Test Plan", amount, RATE_LIMIT, TIME_INTERVAL, _from(owner))
         subscriber_contract.plan_count() == i
@@ -20,7 +20,7 @@ def test_activate_plan(subscriber_contract, accounts):
         subscriber_contract.activate_plan(0, _from(owner))
     for i in range(1, MAX_POOL_CT):
         amount = i*10**18
-        with brownie.reverts("You are not the owner."):
+        with brownie.reverts("Only contract owner can call this function."):
             subscriber_contract.activate_plan(i, _from(bad_guy))
         with brownie.reverts("Plan does not exist."):
             subscriber_contract.activate_plan(i, _from(owner))
@@ -31,7 +31,7 @@ def test_retire_plan(subscriber_contract, accounts):
     owner, bad_guy = accounts[0], accounts[1]
     for i in range(1, MAX_POOL_CT):
         amount = i*10**18
-        with brownie.reverts("You are not the owner."):
+        with brownie.reverts("Only contract owner can call this function."):
             subscriber_contract.retire_plan(i, _from(bad_guy))
         with brownie.reverts("Plan is not active."):
             subscriber_contract.retire_plan(i, _from(owner))
@@ -41,6 +41,6 @@ def test_retire_plan(subscriber_contract, accounts):
 
 def test_retire_contract(subscriber_contract, accounts):
     owner, bad_guy = accounts[0], accounts[1]
-    with brownie.reverts("You are not the owner."):
+    with brownie.reverts("Only contract owner can call this function."):
         subscriber_contract.retire_contract(_from(bad_guy))
     subscriber_contract.retire_contract(_from(owner))
